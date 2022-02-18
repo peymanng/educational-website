5152cbd6ad4e3ddfad6d49a0acb8ee4a97ff76a9
Peyman Golkar <peymanpic00@gmail.com>
2022-02-18 18:46:14 +0330

    added actions

--- courses/admin.py
+++ courses/admin.py
@@ -3,6 +3,18 @@ from .models import Course , Video , Category , Comment
 from jalali_date import datetime2jalali
 from jalali_date.admin import ModelAdminJalaliMixin, StackedInlineJalaliMixin
 
+# ----------actions-----------
+@admin.action(description="غیر فعال کردن دوره ها")
+def inactive_items(modeladmin , request , queryset):
+    result = queryset.update(active=False)
+    modeladmin.message_user(request , f'{result} مورد غیر فعال گردید.')
+
+@admin.action(description="فعال کردن دوره ها")
+def active_items(modeladmin , request , queryset):
+    result = queryset.update(active=True)
+    modeladmin.message_user(request , f'{result} مورد فعال گردید.')
+
+#----------inlines-------------
 class VideoCourseInlines(StackedInlineJalaliMixin,admin.StackedInline):
     model = Video
     extra = 0
@@ -15,6 +27,7 @@ class CourseAdmin(admin.ModelAdmin):
     search_fields = ['title','categories__title','teacher__username','teacher__first_name','teacher__last_name']
     list_editable = ['active' , 'is_finish']
     raw_id_fields = ['teacher']
+    actions = [inactive_items,active_items]
     inlines = [VideoCourseInlines]
 
 
@@ -25,6 +38,8 @@ class CategoryAdmin(admin.ModelAdmin):
     list_editable = ['active']
     list_filter = ['active']
     search_fields = ['title']
+    actions = [inactive_items,active_items]
+
 
 @admin.register(Video)
 class VideoAdmin(ModelAdminJalaliMixin,admin.ModelAdmin):
@@ -32,6 +47,7 @@ class VideoAdmin(ModelAdminJalaliMixin,admin.ModelAdmin):
     list_filter = ['active','publish_time']
     search_fields = ['active','course__title']
     raw_id_fields = ['course']
+    actions = [inactive_items,active_items]
 
 
     def get_publish_time_jalali(self, obj):
@@ -45,6 +61,7 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'active' , 'parent','get_created_jalali')
     list_editable = ('active',)
     list_filter = ('user','active','parent')
+    actions = [inactive_items,active_items]
 
     def get_created_jalali(self, obj):
         return datetime2jalali(obj.created).strftime('%Y/%m/%d _ %H:%M:%S')
