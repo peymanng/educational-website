e49ac8a573d6230550ec1b1c681d9afe8998b5f2
Peyman Golkar <peymanpic00@gmail.com>
2022-02-22 23:42:33 +0330

    added blog models

---
+++ blog/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from ckeditor_uploader.fields import RichTextUploadingField
+from taggit.managers import TaggableManager
+from courses.models import IP
+
+User = get_user_model()
+
+class PostCategory(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField()
+    description = models.CharField(max_length=130, null=True)
+
+    class Meta:
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی ها'
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True, allow_unicode=True)
+    category = models.ManyToManyField(PostCategory)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+    description = models.CharField(max_length=100)
+    body = RichTextUploadingField()
+    pub_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='blogpost_like', blank=True)
+    image = models.ImageField(upload_to='posts/')
+    tags = TaggableManager()
+    visits = models.ManyToManyField(IP, blank=True, related_name='visits')
+
+    class Meta:
+        verbose_name = "مقاله"
+        verbose_name_plural = "مقالات"
+
+    def __str__(self):
+        return self.title
+
+    def get_categories(self):
+        return ' , '.join([category.title for category in self.category.all()])
+
+    get_categories.short_description = "دسته بندی ها"
+
+    def get_visits(self):
+        return self.visits.count()
+
+    get_visits.short_description = 'بازدید'
