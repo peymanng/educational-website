d852af3326ee6311af8039f9680f9f72be1e045d
Peyman Golkar <peymanpic00@gmail.com>
2022-02-24 00:11:22 +0330

    added verbose name to blog models fields

--- blog/models.py
+++ blog/models.py
@@ -7,9 +7,9 @@ from courses.models import IP
 User = get_user_model()
 
 class PostCategory(models.Model):
-    title = models.CharField(max_length=100, unique=True)
+    title = models.CharField(max_length=100, unique=True,verbose_name="عنوان")
     slug = models.SlugField(allow_unicode=True)
-    description = models.CharField(max_length=130, null=True)
+    description = models.CharField(max_length=130, null=True,verbose_name="توضیحات")
 
     class Meta:
         verbose_name = 'دسته بندی'
@@ -19,16 +19,16 @@ class PostCategory(models.Model):
         return self.title
 
 class Post(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50,verbose_name="عنوان")
     slug = models.SlugField(unique=True, allow_unicode=True)
-    category = models.ManyToManyField(PostCategory)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
-    description = models.CharField(max_length=100)
-    body = RichTextUploadingField()
+    category = models.ManyToManyField(PostCategory,verbose_name="دسته بندی")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post',verbose_name="نویسنده")
+    description = models.CharField(max_length=100,verbose_name="توضیحات")
+    body = RichTextUploadingField(verbose_name="متن")
     pub_date = models.DateTimeField(auto_now=False, auto_now_add=True)
-    image = models.ImageField(upload_to='posts/')
-    tags = TaggableManager()
-    visits = models.ManyToManyField(IP, blank=True, related_name='views')
+    image = models.ImageField(upload_to='posts/',verbose_name="عکس")
+    tags = TaggableManager(verbose_name="برچسب ها")
+    visits = models.ManyToManyField(IP, blank=True, related_name='views' ,verbose_name="بازدید ها")
 
     class Meta:
         verbose_name = "مقاله"
